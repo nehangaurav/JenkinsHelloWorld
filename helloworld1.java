9aad251309b29b171ca137445f67814c155fe692
nehangaurav <64072483+nehangaurav@users.noreply.github.com>
2022-01-23 18:28:10 +0530

    Update helloworld1.java

--- helloworld1.java
+++ helloworld1.java
@@ -2,7 +2,7 @@ class helloworld1 {
     public static void main(String[] args) {
         System.out.println("=====================Hello, Welcome to Jenkins Training=================================="); 
         System.out.println("=====================  Trying poll scm==================================");
-            System.out.println("=====================  commit poll scm ==================================");
+            System.out.println("=====================  commit 4 ==================================");
         
     }
 }

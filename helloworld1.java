e492698a926d73367270d480fb40388bc00a71f9
nehangaurav <64072483+nehangaurav@users.noreply.github.com>
2022-01-23 17:02:47 +0530

    uploading files hello world

---
+++ helloworld1.java
@@ -0,0 +1,12 @@
+class helloworld1 {
+    public static void main(String[] args) {
+        System.out.println("=====================Hello, Welcome to Jenkins Training=================================="); 
+        System.out.println("=====================  Trying Webhook1 ==================================");
+        int a=12;
+        int b=32;
+        int c=a+b;
+         System.out.println("=====================  Trying Webhook1 ==================================");
+        
+    }
+}
+

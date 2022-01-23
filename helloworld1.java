3f77d6017f5f6a035324059d0d6eb349ec4038ff
nehangaurav <64072483+nehangaurav@users.noreply.github.com>
2022-01-23 17:26:08 +0530

    Update helloworld1.java
    poll scm added

--- helloworld1.java
+++ helloworld1.java
@@ -1,11 +1,8 @@
 class helloworld1 {
     public static void main(String[] args) {
         System.out.println("=====================Hello, Welcome to Jenkins Training=================================="); 
-        System.out.println("=====================  Trying Webhook1 ==================================");
-        int a=12;
-        int b=32;
-        int c=a+b;
-         System.out.println("=====================  Trying Webhook1 ==================================");
+        System.out.println("=====================  Trying poll scm ==================================");
+            System.out.println("=====================  Trying poll scm ==================================");
         
     }
 }

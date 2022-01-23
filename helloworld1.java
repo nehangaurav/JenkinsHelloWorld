256fb86a4b6252c65a62f58b914b832e29971931
nehangaurav <64072483+nehangaurav@users.noreply.github.com>
2022-01-23 18:04:41 +0530

    Update helloworld1.java
    trying web hook

--- helloworld1.java
+++ helloworld1.java
@@ -1,8 +1,8 @@
 class helloworld1 {
     public static void main(String[] args) {
         System.out.println("=====================Hello, Welcome to Jenkins Training=================================="); 
-        System.out.println("=====================  Trying poll scm ==================================");
-            System.out.println("=====================  Trying poll scm ==================================");
+        System.out.println("=====================  Trying pweb hook==================================");
+            System.out.println("=====================  Trying web hook ==================================");
         
     }
 }

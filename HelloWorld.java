dded089a57030697a38ed72c3b9895711348fedb
nehangaurav <64072483+nehangaurav@users.noreply.github.com>
2022-01-23 16:03:05 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("=====================Hello, Welcome to Jenkins Training=================================="); 
-        System.out.println("=====================  Trying Webhook ==================================");
+        System.out.println("=====================  Trying Webhook1 ==================================");
+        int a=12;
+        int b=32;
+        c=a+b;
+         System.out.println("===================== c Trying Webhook1 ==================================");
+        
     }
 }
